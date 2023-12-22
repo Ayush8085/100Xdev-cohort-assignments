7f4755442965c2a8cd4f28467feab2175273d9b4
Ayush <ayushkumar9601@gmail.com>
2023-12-22 14:57:38 +0530

    week-3/01-middlewares complete

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -13,14 +13,31 @@ const app = express();
 
 let numberOfRequestsForUser = {};
 setInterval(() => {
-    numberOfRequestsForUser = {};
+  numberOfRequestsForUser = {};
 }, 1000)
 
-app.get('/user', function(req, res) {
+app.use((req, res, next) => {
+  const userId = req.header['user-id'];
+
+  if (numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser[userId] += 1;
+    if (numberOfRequestsForUser[userId] >= 5) {
+      return res.status(404).send();
+    }
+    next();
+  }
+  else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
+  }
+
+});
+
+app.get('/user', function (req, res) {
   res.status(200).json({ name: 'john' });
 });
 
-app.post('/user', function(req, res) {
+app.post('/user', function (req, res) {
   res.status(200).json({ msg: 'created dummy user' });
 });
 
