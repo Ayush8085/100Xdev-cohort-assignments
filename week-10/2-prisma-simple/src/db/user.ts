8b1611fffef6b93f97db336233440b18ae8df128
Ayush <ayushkumar9601@gmail.com>
2024-02-23 14:47:09 +0530

    week-10/2-simple-prisma complete

---
+++ week-10/2-prisma-simple/src/db/user.ts
@@ -0,0 +1,47 @@
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+
+/*
+ * Should insert into the users table
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function createUser(username: string, password: string, name: string) {
+    const user = await prisma.user.create({
+        data: {
+            username,
+            password,
+            name
+        }
+    });
+
+    console.log('CREATE USER: ', user);
+
+    return user;
+}
+
+/*
+ * Should return the User object
+ * {
+ *   username: string,
+ *   password: string,
+ *   name: string
+ * }
+ */
+export async function getUser(userId: number) {
+    const user = await prisma.user.findFirst({
+        where: {
+            id: userId,
+        }
+    });
+
+    console.log('GET USER: ', user);
+
+    return user;    
+}
